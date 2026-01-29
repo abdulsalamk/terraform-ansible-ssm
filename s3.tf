daf29b73ecd13abc0386b776deae0db1c04f6f20
Abdul Kunnummal <abdul.kunnummal@gmail.com>
2026-01-29 20:20:10 +0000

    Initial commit

--- s3.tf
+++ s3.tf
@@ -1,7 +1,7 @@
 resource "aws_s3_bucket" "main" {
-  bucket = "ansible-bucket-4-demo"
+  bucket = "ansible-bucket-9-demo"
 
   tags = merge({
-    Name = "ansible-bucket-4-demo",
+    Name = "ansible-bucket-9-demo",
   }, local.default_tags)
 }
