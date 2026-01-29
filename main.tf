6b618bffe365d0c2fd519d2741306f406bc0e52c
Abdul Kunnummal <abdul.kunnummal@gmail.com>
2026-01-29 15:07:43 +0000

    Initial commit

--- main.tf
+++ main.tf
@@ -51,7 +51,7 @@ resource "aws_iam_role_policy_attachment" "main" {
 resource "aws_security_group" "main" {
   name        = "my-instance-sg"
   description = "Security group for private bastion instance"
-  vpc_id      = aws_default_subnet.az1.vpc_id
+  vpc_id      = aws_vpc.project-vpc.id
   tags        = local.default_tags
 }
 
