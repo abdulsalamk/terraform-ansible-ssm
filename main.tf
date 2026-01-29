fac1c1e25203d344bcdca2286a2289a8da014adb
Abdul Kunnummal <abdul.kunnummal@gmail.com>
2026-01-29 15:24:08 +0000

    Initial commit

--- main.tf
+++ main.tf
@@ -85,7 +85,7 @@ resource "aws_security_group_rule" "ingress_http" {
 
 resource "aws_instance" "main" {
   ami                         = data.aws_ami.ubuntu_22_04_server_amd_64.id
-  instance_type               = "t4g.micro"
+  instance_type               = "t3.micro"
   vpc_security_group_ids      = [aws_security_group.main.id]
   subnet_id                   = data.aws_subnets.available.ids[0]
   user_data_replace_on_change = true
